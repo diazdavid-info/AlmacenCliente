8e5bf7b2e638c1f459041d5ed999855c2d1f5418
David Díaz <diazdavid.info@gmail.com>
2015-02-18 08:42:13 +0100

    Signed-off-by: David Díaz <diazdavid.info@gmail.com>

--- ClientAlmacen/function.js
+++ ClientAlmacen/function.js
@@ -7,12 +7,40 @@ window.onload = function(){
 		e.preventDefault()
 		$(this).tab('show')
 	});
+	init_app();
 }
 
-//$.ajax({
-//	url: 'http://localhost:8080/Almacen/services/putServicesImple',
-//	type: '',
-//	data: '',
-//	dataType: ''
-//});
+function init_app(){
+	requestServices({typeRequest: 'getAllCompanies'}, printCompanies);
+	
+	startEvent();
+}
 
+function startEvent(){
+	$('#sendCompany').click(sendCompany);
+}
+
+function sendCompany(e){
+	console.log(e);
+}
+
+function printCompanies(response){
+	var json = JSON.parse(response);//selectCompay
+	$.each(json, function(k,v){
+		$('#selectCompay').append('<option value='+v.mId+'>'+v.mName+'</option>');
+	});
+}
+
+function requestServices(params, callBack){
+	$.ajax({
+		url: 'services.php',
+		type: 'GET',
+		data: params,
+		success: callBack,
+		error: callBackError
+	})
+}
+
+function callBackError(){
+	console.log("Error de llamada");
+}
---
+++ ClientAlmacen/services.php
@@ -0,0 +1,55 @@
+<?php
+$service_url = "http://localhost:8080/Almacen/webServices/rest/";
+$method_name = "GET";
+
+switch ($_GET["typeRequest"]){
+	case "getAllCompanies":
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, array('Accept: application/json'));
+		curl_setopt($ch, CURLOPT_URL, $service_url . "getAllCompanies");
+		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+		$api_response = curl_exec($ch);
+		$api_response_info = curl_getinfo($ch);
+		curl_close($ch);
+		
+		$api_response_header = trim(substr($api_response, 0, $api_response_info['header_size']));
+		$api_response_body = substr($api_response, $api_response_info['header_size']);
+		
+		// Response HTTP Status Code
+		//echo $api_response_info['http_code'];
+		
+		// Response Header
+		//echo $api_response_header;
+		
+		// Response Body
+		echo $api_response_body;
+		break;
+	case "sendCompany":
+		$api_request_parameters = array('nameCompany' => 'Todo perfecto');
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+		curl_setopt($ch, CURLOPT_HEADER, array('Accept: application/json'));
+		curl_setopt($ch, CURLOPT_URL, $service_url . "saveCompany?nameCompany=todoOK");
+		
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($api_request_parameters));
+		curl_setopt($ch, CURLOPT_HEADER, TRUE);
+		$api_response = curl_exec($ch);
+		$api_response_info = curl_getinfo($ch);
+		//var_dump($ch);
+		curl_close($ch);
+		
+		$api_response_header = trim(substr($api_response, 0, $api_response_info['header_size']));
+		$api_response_body = substr($api_response, $api_response_info['header_size']);
+		
+		// Response HTTP Status Code
+		//echo $api_response_info['http_code'];
+		
+		// Response Header
+		//echo $api_response_header;
+		
+		// Response Body
+		echo $api_response_body;
+		break;
+}
